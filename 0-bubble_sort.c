7dbcc4bca8ffbe5e7e14614f3c11e5e63391dec9
UmarMsani <umuhammad220@gmail.com>
2023-09-25 02:36:16 -0700

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0; /*Flag to check if any swaps were made in this pass*/
 
 		for (j = 0; j < size - 1 - i; j++)
 		{
@@ -24,13 +22,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1; /*Set the flag to true if a swap was made*/
 				print_array(array, size);
 			}
 		}
-
-		/*If no swaps were made in this pass, the array is already sorted*/
-		if (swapped == 0)
-			break;
 	}
 }
